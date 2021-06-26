149932150129b398932ae6c2e3da9e484a896460
acrazyczy <a_crazy_czy@yahoo.com>
2021-06-26 20:24:50 +0800

    sret lost its way in Wonderland

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,7 +9,7 @@ fi
 echo "compiling kernel ..."
 cd build
 
-cmake -DCMAKE_BUILD_TYPE=CMAKE_C_FLAGS_DEBUG -DCMAKE_LINKER=riscv64-linux-gnu-ld -DCMAKE_C_COMPILER=riscv64-linux-gnu-gcc -DCMAKE_ASM_COMPILER=riscv64-linux-gnu-gcc -DCMAKE_C_LINK_EXECUTABLE="<CMAKE_LINKER> <LINK_FLAGS> <OBJECTS> -o <TARGET> <LINK_LIBRARIES>" .. -G "Unix Makefiles" "$@"
+cmake -DCMAKE_BUILD_TYPE=CMAKE_C_FLAGS_RELEASE -DCMAKE_LINKER=riscv64-linux-gnu-ld -DCMAKE_C_COMPILER=riscv64-linux-gnu-gcc -DCMAKE_ASM_COMPILER=riscv64-linux-gnu-gcc -DCMAKE_C_LINK_EXECUTABLE="<CMAKE_LINKER> <LINK_FLAGS> <OBJECTS> -o <TARGET> <LINK_LIBRARIES>" .. -G "Unix Makefiles" "$@"
 
 echo "before make"
 make
